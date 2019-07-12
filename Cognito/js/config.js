21aac80fa6a3e3399aa171684b7cece9e339320d
Harshavardhan Koneru <koneru.harshavardhan499@gmail.com>
2019-07-12 12:46:04 +0530

    Add files via upload

---
+++ Cognito/js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_4BmYEkobE',
+        region: 'us-east-1',
+        clientId: 'bcgto49u0iskhm5vtme4am35h'
+    },
+    urls: {
+        arrowProductsURL: 'https://arrow-products.s3.amazonaws.com/arrowproducts.html'
+    }   
+}
