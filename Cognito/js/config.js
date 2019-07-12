b61f42a3ec2feb025ca45ac48cf3440c3629f836
Harshavardhan Koneru <koneru.harshavardhan499@gmail.com>
2019-07-12 13:26:24 +0530

    URLs modification

--- Cognito/js/config.js
+++ Cognito/js/config.js
@@ -5,6 +5,10 @@ window._config = {
         clientId: 'bcgto49u0iskhm5vtme4am35h'
     },
     urls: {
-        arrowProductsURL: 'https://arrow-products.s3.amazonaws.com/arrowproducts.html'
+        arrowProductsURL: 'https://khv499.github.io/AWS/Cognito/app/arrowproducts.html',
+        signInURL: 'https://khv499.github.io/AWS/Cognito/signIn.html',
+        signUpURL: 'https://khv499.github.io/AWS/Cognito/signUp.html',
+        forgotPasswordURL: 'https://khv499.github.io/AWS/Cognito/forgotPassword.html',
+        confirmForgotPasswordURL: 'https://khv499.github.io/AWS/Cognito/confirmForgotPassword.html'
     }   
 }
